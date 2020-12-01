26bc7de7e45d5c0f37df146fdf1721179cae451e
Tetiana Yatsenko <tetiana.ua@gmail.com>
2020-12-01 19:17:15 +0100

    Добавляет 6 задание

---
+++ js/task-6.js
@@ -0,0 +1,23 @@
+let total = 0;
+
+while (true) {
+    let userInput = prompt("Введите число");
+
+    if (userInput === null) {
+        console.log("Отмена пользователем");
+        break;
+    }
+
+    userInput = Number(userInput);
+
+    const notANumber = Number.isNaN(userInput);
+
+    if (notANumber) {
+        console.log("Было введено не число, пропускаем итерацию!");
+        continue;
+    }
+
+    total += userInput;
+}
+
+console.log(`Общая сумма чисел равна ${total}`)
