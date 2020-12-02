49b8bfcf0e674035780d19418f013919ada95a16
Tetiana Yatsenko <tetiana.ua@gmail.com>
2020-12-02 09:47:59 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,23 +1,24 @@
 let credits = 23580;
 const pricePerDroid = 3000;
 let userInput = prompt("Введите желаемое количество дроидов");
-let message;
 let totalPrice;
 
 
 if (userInput === null) {
-    message = "Отменено пользователем!";
+    console.log("Отменено пользователем!");
 } else {
     userInput = Number(userInput);    
-    totalPrice = userInput * pricePerDroid;        
+    totalPrice = userInput * pricePerDroid;   
+    if (totalPrice > credits) {
+    console.log("Недостаточно средств на счету!");
+    } else {
+        credits = credits - totalPrice;
+        console.log(`Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`)
+    }    
 } 
 
-if (totalPrice > credits) {
-    console.log("Недостаточно средств на счету!");
-} else {
-    credits = credits - totalPrice;
-    console.log(`Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`)
-}
+
+
 
 
 
