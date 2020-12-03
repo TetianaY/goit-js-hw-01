ff7098eb3cdd1ef238aa3a28a6fb721d8a626a63
Tetiana Yatsenko <tetiana.ua@gmail.com>
2020-12-03 15:20:50 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -30,24 +30,20 @@ do {
     userInput = prompt("Введите число");
 
     if (userInput === null) {
-        console.log("Отмена пользователем");
-        break;
-    }
+        console.log("Отмена пользователем");        
+    } else {
 
-    userInput = Number(userInput);
+        userInput = Number(userInput);
+        const notANumber = Number.isNaN(userInput);
 
-    const notANumber = Number.isNaN(userInput);
-
-    if (notANumber) {
-        console.log("Было введено не число, попробуйте еще раз");
-        continue;
-    }
-
-
+        if (notANumber) {
+            console.log("Было введено не число, попробуйте еще раз");        
+        }
+    }    
 }
 
 while (userInput !== null) {    
     total += userInput;
+    console.log(`Общая сумма чисел равна ${total}`)
 }
 
-console.log(`Общая сумма чисел равна ${total}`)
